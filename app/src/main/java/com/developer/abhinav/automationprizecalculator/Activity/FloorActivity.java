2b36973bce7ae34e788be463196b24624725a220
Abhinav  Raj <abhinav.23.april@gmail.com>
2018-10-22 20:27:01 +0530

    Initial commit

---
+++ app/src/main/java/com/developer/abhinav/automationprizecalculator/Activity/FloorActivity.java
@@ -0,0 +1,16 @@
+package com.developer.abhinav.automationprizecalculator.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.developer.abhinav.automationprizecalculator.R;
+
+public class FloorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floor);
+
+    }
+}
